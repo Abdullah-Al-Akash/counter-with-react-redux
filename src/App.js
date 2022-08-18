e5f4468b6212eb66ab3957bab89b54b9c756e879
Abdullah Al Akash <aabdullahalakash@gmail.com>
2022-08-18 20:28:44 +0600

    Complete Counter Application

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import Counter from './components/Counter';
+import { Provider } from 'react-redux';
+import store from './redux/store';
 
 function App() {
   return (
     <div className="App">
-      <Counter></Counter>
+      <Provider store={store}>
+        <Counter></Counter>
+      </Provider>
     </div>
   );
 }
